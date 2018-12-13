88a7aab42e0845e65220710b5538ae5a2381106e
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2018-12-13 12:38:38 +0100

    Add Routes and resources

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'cart',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/img/cart_icon.svg')
+    );
+  }
 }
