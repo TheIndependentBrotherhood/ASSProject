91a1a64b17f72bea2cd90b6ed7996adeaff8c875
The Independent Brotherhood <theindependentbrotherhood@gmail.com>
2019-01-28 14:40:14 +0100

    Connecting Angular to the Backend

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +6,5 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
-  ) {
-    this.matIconRegistry.addSvgIcon(
-      'cart',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/img/cart_icon.svg')
-    );
-  }
+
 }
